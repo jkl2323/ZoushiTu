11a4c4f81245db798e99162b8d888f3cfad4ad35
jkl2323 <335702611@qq.com>
2018-09-07 15:09:20 +0800

    第一次提交

---
+++ app/src/main/java/com/example/administrator/zoushitu/ChartView.java
@@ -0,0 +1,240 @@
+package com.example.administrator.zoushitu;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.View;
+
+import java.util.ArrayList;
+
+public class ChartView extends View {
+    private int mBallWH;
+    //红球个数:33个
+    private int mRedNum=33;
+
+    //蓝球个数:16个
+    private int mBlueNum=16;
+    //网格的水平间距
+    private float mDeltaX;
+    //网格垂直间距
+    private float mDeltaY;
+    //当前View的宽度
+    private int mWidth;
+
+    //当前View的高度
+    private int mHeight;
+    ArrayList<String[]> ballList=new ArrayList<>();
+    ArrayList<Integer> choseList=new ArrayList<>();
+    ArrayList<PointRecord> list_data=new ArrayList<>();
+    private Paint mPaintLine;
+    private Paint mPaintBall;
+    private Paint mPaintText;
+    private Paint mPainTextBall;
+    private Paint mPaintLinkLine;
+
+    public ChartView(Context context) {
+        super(context);
+        init();
+    }
+
+    public ChartView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public ChartView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+    public void init(){
+        mBallWH=getResources().getDimensionPixelSize(R.dimen.trend_ball_wh);
+        //设置单个网格的水平和垂直间距
+        this.mDeltaY=mBallWH*2f;
+        Log.i("delta","deltay:"+mDeltaY);//高度;50
+        this.mDeltaX=this.mDeltaY;
+        ballList.clear();
+        initLinePaint();
+        for (int i = 0; i < 16; i++) {
+            String[] str=new String[10];
+            for (int j = 0; j < 10; j++) {
+                str[j]=String.valueOf(j);
+            }
+            ballList.add(str);
+        }
+        choseList.add(1);
+        choseList.add(9);
+        choseList.add(2);
+        choseList.add(4);
+        choseList.add(6);
+        choseList.add(9);
+        choseList.add(8);
+        choseList.add(3);
+        choseList.add(4);
+        choseList.add(3);
+        choseList.add(8);
+        choseList.add(3);
+        choseList.add(9);
+        choseList.add(3);
+        choseList.add(3);
+        choseList.add(14);
+    }
+
+    private void initLinePaint() {
+        int dpValue=getScreenDenisty();
+        //网格线画笔
+        mPaintLine=new Paint();
+        mPaintLine.setColor(Color.GRAY);
+        mPaintLine.setAntiAlias(true);
+        mPaintLine.setStrokeWidth(dpValue*0.6f/160);
+
+        mPaintBall = new Paint();
+        mPaintBall.setColor(Color.RED);
+        mPaintBall.setAntiAlias(true);
+
+
+        //普通文字画笔
+        mPaintText=new Paint();
+        mPaintText.setColor(Color.BLACK);
+        mPaintText.setTextSize((dpValue*12/160));
+        mPaintText.setAntiAlias(true);
+        mPaintText.setStrokeWidth(2f);
+        mPaintText.setTextAlign(Paint.Align.CENTER);
+
+        //小球上面的文字画笔
+        mPainTextBall=new Paint();
+        mPainTextBall.setColor(Color.WHITE);
+        mPainTextBall.setTextSize((dpValue*12/160));
+        mPainTextBall.setAntiAlias(true);
+        mPainTextBall.setStrokeWidth(2f);
+        mPainTextBall.setTextAlign(Paint.Align.CENTER);
+
+        mPaintLinkLine=new Paint();
+        mPaintLinkLine.setColor(Color.GREEN);
+        mPaintLinkLine.setTextSize((dpValue*12/160));
+        mPaintLinkLine.setAntiAlias(true);
+        mPaintLinkLine.setStrokeWidth(8f);
+        mPaintLinkLine.setTextAlign(Paint.Align.CENTER);
+    }
+
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        setMeasuredDimension((int) ((mRedNum) * mDeltaX), (int) (16 * mDeltaY));
+        //取得测量之后当前View的宽度
+        this.mWidth = getMeasuredWidth();
+        //取得测量之后当前View的高度
+        this.mHeight = getMeasuredHeight();
+        //重新绘制,不重绘,不会生效;
+//        invalidate();
+    }
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        drawXY(canvas);
+        drawLin(canvas);
+        drawText(canvas); }
+
+    private void drawLin(Canvas canvas) {
+        for (int i = 0; i < choseList.size(); i++) {
+            Integer number = choseList.get(i);
+            if (i<choseList.size()-1) {
+                Integer next_number = choseList.get(i + 1);
+                float x = number * mDeltaX + (mDeltaX / 2);
+                float y = i * mDeltaX + (mDeltaY / 2);
+                float next_x = next_number * mDeltaX + (mDeltaX / 2);
+                float next_y = (i+1) * mDeltaX + (mDeltaY / 2);
+                canvas.drawLine(x, y, next_x, next_y, mPaintLinkLine);
+            }
+        }
+    }
+    private void drawText(Canvas canvas) {
+        for (int i = 0; i < 16; i++) {
+            Integer choseNumber = choseList.get(i);
+            for (int j = 0; j < 10; j++) {
+                float x=mDeltaX*j+(mDeltaX/2);
+                float y=mDeltaY*i+(mDeltaY/2);
+                if (choseNumber==j){
+                canvas.drawCircle(x,y,mDeltaX/2,mPaintBall);
+                }
+                float text_y = mPaintText.measureText(String.valueOf(i));
+                canvas.drawText(String.valueOf(j),x,y+(text_y/2),choseNumber==j?mPainTextBall:mPaintText);
+            }
+        }
+    }
+
+
+    private void drawXY(Canvas canvas) {
+        for (int i = 0; i < 10; i++) {//画y轴
+            canvas.drawLine(mDeltaX*i,0,mDeltaX*i,mHeight,mPaintLine);
+        }
+        for (int i = 0; i < 16; i++) {//画x轴
+            canvas.drawLine(0,mDeltaY*i,mWidth,mDeltaY*i,mPaintLine);
+        }
+
+    }
+
+    /**
+     * 获取当前屏幕的密度
+     * @return
+     */
+    public int getScreenDenisty(){
+        DisplayMetrics dm=getResources().getDisplayMetrics();
+        return dm.densityDpi;
+    }
+    private class PointRecord{//记录圆圈左右坐标
+
+        private float[] left;
+        private float[] right;
+        private float[] top;
+        private float[] bottom;
+        private int numbers;//选中的号码
+
+        public float[] getLeft() {
+            return left;
+        }
+
+        public void setLeft(float[] left) {
+            this.left = left;
+        }
+
+        public float[] getRight() {
+            return right;
+        }
+
+        public void setRight(float[] right) {
+            this.right = right;
+        }
+
+        public float[] getTop() {
+            return top;
+        }
+
+        public void setTop(float[] top) {
+            this.top = top;
+        }
+
+        public float[] getBottom() {
+            return bottom;
+        }
+
+        public void setBottom(float[] bottom) {
+            this.bottom = bottom;
+        }
+
+        public int getNumbers() {
+            return numbers;
+        }
+
+        public void setNumbers(int numbers) {
+            this.numbers = numbers;
+        }
+    }
+}
